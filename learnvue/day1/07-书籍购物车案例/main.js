e25439f6846f98e62efb8fc216703f753dba8581
2268849815 <2268849815@qq.com>
2020-03-16 11:22:01 +0800

    lesson_shuidi

---
+++ learnvue/day1/07-书籍购物车案例/main.js
@@ -0,0 +1,60 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    books: [
+      {
+        id: 1,
+        name: '《算法导论》',
+        data: '2006-9',
+        price: '85',
+        count:1
+      },
+      {
+        id: 2,
+        name: '《UNIX编程艺术》',
+        data: '2006-2',
+        price: '59',
+        count:1
+      },
+      {
+        id: 3,
+        name: '《编程珠玑》',
+        data: '2008-10',
+        price: '39',
+        count:1
+      },
+      {
+        id: 3,
+        name: '《代码大全》',
+        data: '2006-3',
+        price: '128',
+        count:1
+      },
+    ]
+  },
+  methods:{
+    getFinalPrice(price) {
+      // return '￥' + price.toFixed(1)
+    },
+    remove (index) {
+      this.books.splice(index,1)
+    } 
+  },
+  filters:{
+    // showPrice(price) {
+    //   return '￥' + price.toFixed(2)
+    // }
+  },
+  computed:{
+    totilPrice() {
+      // let sum = 0
+      // for (let i = 0; i < this.books.length; i++) {
+      //    sum += this.books[i].price * this.books[i].count
+      // }
+      // return sum
+      for (let i in books) {
+        console.log(i)
+      }
+    }
+  }
+})
