442577ae9071fa5d97fb88652dc9da94ad95a045
2268849815 <2268849815@qq.com>
2020-03-25 22:13:45 +0800

    learnvue

---
+++ learnvue/day4/01-webpack的使用/02-webpack的配置/src/main.js
@@ -0,0 +1,10 @@
+// const {add, mul} = require('./mathUtils.js')
+// console.log(add(20, 30))
+
+// import {name} from './info'
+// console.log(name)
+
+require('./css/normal.css')
+
+require('./css/special.less')
+document.writeln('<h2>你好啊</h2>')
---
+++ learnvue/day4/01-webpack的使用/02-webpack的配置/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.less$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "less-loader" // compiles Less to CSS
+            }]
+      }
+    ]
+  }
+}
