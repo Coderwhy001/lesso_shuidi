d4532ed77a0279f6389bd517b0c69b74ae7a337e
2268849815 <2268849815@qq.com>
2020-04-01 13:51:05 +0800

    lesson_shuidi

---
+++ leet_code/有效括号的嵌套深度/1.js
@@ -0,0 +1,15 @@
+var maxDepthAfterSplit = function(seq) {
+  let dep = 0;
+  return seq.split("").map((value, index) => {
+    if (value === '(') {
+      dep++;
+      return dep % 2
+    } else {
+      let ans = dep % 2;
+      dep--;
+      return ans
+    }
+  })
+};
+
+console.log(maxDepthAfterSplit("(()())"))
