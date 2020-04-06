f4817475c520a8858a814d060f996cdbd8968651
2268849815 <2268849815@qq.com>
2020-04-06 19:44:00 +0800

    leet_code

---
+++ leet_code/编辑距离/1.js
@@ -0,0 +1,22 @@
+var minDistance = function(word1, word2) {
+  let m = word1.length
+  let n = word2.length
+  let dp = Array.from(new Array(m+1),() => new Array(n+1).fill(0));
+  for (let i = 1; i <= m; i++) {
+    dp[i][0] = dp[i-1][0] + 1
+  }
+  for (let j = 1; j <= n; j++) {
+    dp[0][j] = dp[0][j-1] + 1
+  }
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (word1[i-1] === word2[j-1]) {
+        dp[i][j] = dp[i-1][j-1]
+      } else {
+        dp[i][j] = Math.min(dp[i-1][j-1], dp[i][j-1], dp[i-1][j]) + 1;
+      }
+    }
+  }
+  return dp[m][n]
+};
+console.log(minDistance(word1 = "horse", word2 = "ros"))
