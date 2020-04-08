d738af08022ebfcb697bbaa8bcb37eb040a08daf
2268849815 <2268849815@qq.com>
2020-04-08 12:41:31 +0800

    1

--- leet_code/圆圈中最后剩下的数字/1.js
+++
@@ -1,7 +0,0 @@
-var lastRemaining = function(n, m) {
-  let result = 0;
-  for(let i = 2; i <= n; i++) {
-      result = (m + result) % i;
-  }
-  return result
-};
--- leet_code/排序数组/1.js
+++
@@ -1,4 +0,0 @@
-var sortArray = function(nums) {
-    return nums.sort((a, b) => a - b)
-};
-console.log(sortArray([5,1,1,2,0,0]))
