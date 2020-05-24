89f096ddc5ff343dca8b186ec42c842f288c5c91
2268849815 <2268849815@qq.com>
2020-05-24 21:19:00 +0800

    electron

--- electron/electron-quick-start/main.js
+++ electron/electron-quick-start/main.js
@@ -1,21 +1,28 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 
 app.on('ready', () => {
+  require('devtron').install()
   let mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1600,
+    height: 1200,
     webPreferences: {
       nodeIntegration: true
     }
   })
   mainWindow.loadFile('index.html')
-  let secondWindow = new BrowserWindow({
-    width: 400,
-    height: 300,
-    webPreferences: {
-      nodeIntegration: true
-    },
-    parent: mainWindow
+  mainWindow.webContents.openDevTools()
+  ipcMain.on('message', (event, arg) => {
+    console.log(arg)
+    console.log(event)
+    event.reply('reply', 'hello from main process')
   })
-  secondWindow.loadFile('second.html')
+  // let secondWindow = new BrowserWindow({
+  //   width: 400,
+  //   height: 300,
+  //   webPreferences: {
+  //     nodeIntegration: true
+  //   },
+  //   parent: mainWindow
+  // })
+  // secondWindow.loadFile('second.html')
 })
--- electron/electron-quick-start/renderer.js
+++ electron/electron-quick-start/renderer.js
@@ -4,7 +4,17 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+const { ipcRenderer } = require('electron')
+const { BrowserWindow } = require('electron').remote
 
 window.addEventListener('DOMContentLoaded', () => {
   document.getElementById('node-version').innerHTML = process.versions.node
+  document.getElementById('send').addEventListener('click', () => {
+    ipcRenderer.send('message', 'hello fron renderer')
+    let win = new BrowserWindow({ width: 800, height: 600 })
+    win.loadURL('http://baidu.com')
+  })
+  ipcRenderer.on('reply', (event, arg) => {
+    document.getElementById('message').innerHTML = arg
+  })
 })
